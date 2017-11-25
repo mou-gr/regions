e8407a01f23e930a142c184e8a7244af7cd41478
apanagio <apanagio@gmail.com>
2017-11-25 16:02:33 +0200

    linting list.js

--- invitation/public/list.js
+++ invitation/public/list.js
@@ -1,118 +1,122 @@
+/*global $*/
 $(document).ready(function() {
     $('[data-toggle=confirmation]').confirmation({
-      rootSelector: '[data-toggle=confirmation]',
-      // other options
-    });
-    $("#new-invitation").on('click', function() {
-        const value = $("#fieldName").val();
+        rootSelector: '[data-toggle=confirmation]',
+        // other options
+    })
+    $('#new-invitation').on('click', function() {
+        const value = $('#fieldName').val()
 
-        if (value === "") {
+        if (value === '') {
             return
         }
         $.ajax({
-            url: "/invitation",
-            type: "POST",
-            dataType: "json",
-            data: JSON.stringify({name: value}),
-            contentType: "application/json",
+            url: '/invitation',
+            type: 'POST',
+            dataType: 'json',
+            data: JSON.stringify({
+                name: value
+            }),
+            contentType: 'application/json',
             cache: false,
             timeout: 5000,
             complete: function() {
-                console.log('process complete');
-                location.reload();
+                console.log('process complete')
+                location.reload()
             },
             success: function(data) {
-                console.log(data);
-                console.log('process sucess');
+                console.log(data)
+                console.log('process sucess')
             },
             error: function() {
-                console.log('process error');
+                console.log('process error')
             }
-        });
-    });
-    $(".rename-btn").on('click', function() {
-        const value = $(this).closest('tr').find('.rename-text').val();
-        if (value === "") {
+        })
+    })
+    $('.rename-btn').on('click', function() {
+        const value = $(this).closest('tr').find('.rename-text').val()
+        if (value === '') {
             return
         }
-        const id = $(this).closest('tr').find('.id').text();
+        const id = $(this).closest('tr').find('.id').text()
         $.ajax({
-            url: "/api/invitation/" + id + '/rename',
-            type: "PUT",
-            dataType: "json",
-            data: JSON.stringify({name: value}),
-            contentType: "application/json",
+            url: '/api/invitation/' + id + '/rename',
+            type: 'PUT',
+            dataType: 'json',
+            data: JSON.stringify({
+                name: value
+            }),
+            contentType: 'application/json',
             cache: false,
             timeout: 5000,
             complete: function(resp, status, ) {
-                if (resp.responseJSON.data == "OK") {
-                    location.reload();
+                if (resp.responseJSON.data == 'OK') {
+                    location.reload()
                 } else {
-                    console.log(resp, status, 'process error');
+                    console.log(resp, status, 'process error')
                 }
-            },
-        });
-    });
-   $(".final-checkbox").on('change', function() {
-        const value = $(this).prop('checked');
+            }
+        })
+    })
+    $('.final-checkbox').on('change', function() {
+        const value = $(this).prop('checked')
         const sendVal = value ? 1 : 0
-        const id = $(this).closest('tr').find('.id').text();
+        const id = $(this).closest('tr').find('.id').text()
         $.ajax({
-            url: "/api/invitation/" + id + '/final',
-            type: "PUT",
-            dataType: "json",
-            data: JSON.stringify({IsFinal: sendVal}),
-            contentType: "application/json",
+            url: '/api/invitation/' + id + '/final',
+            type: 'PUT',
+            dataType: 'json',
+            data: JSON.stringify({
+                IsFinal: sendVal
+            }),
+            contentType: 'application/json',
             cache: false,
             timeout: 5000,
             complete: function(resp, status, ) {
-                if (resp.responseJSON.data == "OK") {
-                    location.reload();
+                if (resp.responseJSON.data == 'OK') {
+                    location.reload()
                 } else {
-                    console.log(resp, status, 'process error');
+                    console.log(resp, status, 'process error')
                 }
-            },
-        });
-    });
-   $(".clone-btn").on('click', function() {
-        const id = $(this).closest('tr').find('.id').text();
+            }
+        })
+    })
+    $('.clone-btn').on('click', function() {
+        const id = $(this).closest('tr').find('.id').text()
         $.ajax({
-            url: "/api/invitation/" + id + '/clone',
-            type: "POST",
-            dataType: "json",
-            data: "{}",
-            contentType: "application/json",
+            url: '/api/invitation/' + id + '/clone',
+            type: 'POST',
+            dataType: 'json',
+            data: '{}',
+            contentType: 'application/json',
             cache: false,
             timeout: 5000,
             complete: function(resp, status, ) {
-                if (resp.responseJSON.data == "OK") {
-                    location.reload();
+                if (resp.responseJSON.data == 'OK') {
+                    location.reload()
                 } else {
-                    console.log(resp, status, 'process error');
+                    console.log(resp, status, 'process error')
                 }
             },
-        });
-    });
-   $(".remove-btn").on('click', function() {
-        const id = $(this).closest('tr').find('.id').text();
+        })
+    })
+    $('.remove-btn').on('click', function() {
+        const id = $(this).closest('tr').find('.id').text()
         $.ajax({
-            url: "/api/invitation/" + id,
-            type: "DELETE",
-            dataType: "json",
-            data: "{}",
-            contentType: "application/json",
+            url: '/api/invitation/' + id,
+            type: 'DELETE',
+            dataType: 'json',
+            data: '{}',
+            contentType: 'application/json',
             cache: false,
             timeout: 5000,
             complete: function(resp, status, ) {
-                if (resp.responseJSON.data == "OK") {
-                    location.reload();
+                if (resp.responseJSON.data == 'OK') {
+                    location.reload()
                 } else {
-                    console.log(resp, status, 'process error');
+                    console.log(resp, status, 'process error')
                 }
-            },
-        });
-    });
-
-
-
+            }
+        })
+    })
 })
